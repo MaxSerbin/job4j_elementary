8d04c339ea54a7e29136bc35c3ce95bc0072ce40
MaxSerbin <silverspoor644@gmail.com>
2020-11-14 19:47:24 +0300

    6. Идеальный вес [#406799]

---
+++ src/ru/job4j/calculator/Fit.java
@@ -0,0 +1,20 @@
+package ru.job4j.calculator;
+
+public class Fit {
+
+    public static double manWeight(double height) {
+        return (height - 100) * 1.15;
+    }
+    public static double womanWeight(double height) {
+        return (height - 110) * 1.15;
+    }
+
+  public static void main(String[] args) {
+        double man = Fit.manWeight(186);
+        System.out.println("Man 186 is " + man);
+        double woman = Fit.womanWeight(165);
+        System.out.println("Woman 165 is " + woman);
+
+
+  }
+}
