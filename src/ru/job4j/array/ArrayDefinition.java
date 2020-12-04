931024829c9656c1533cad4047a999014da9f0b7
MaxSerbin <silverspoor644@gmail.com>
2020-12-04 18:50:51 +0300

    6.0.2. Заполнение массива. [#406780]

--- src/ru/job4j/array/ArrayDefinition.java
+++ src/ru/job4j/array/ArrayDefinition.java
@@ -8,5 +8,14 @@ public class ArrayDefinition {
         System.out.println("Размер массива равен : " + surnames.length);
         float[] prices = new float[40];
         System.out.println("Размер массива равен : " + prices.length);
+        String[] names = new String[4];
+        names[0] = "Иван Иванов";
+        names[1] = "Петр Петров";
+        names[2] = "Юсуби Энамото";
+        names[3] = "Роб Зомби";
+        System.out.println(names[0]);
+        System.out.println(names[1]);
+        System.out.println(names[2]);
+        System.out.println(names[3]);
     }
 }
